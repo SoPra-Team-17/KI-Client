a5d3ca03612c7ed61fa3e4d98b732180a3ee097b
csacro <50425802+csacro@users.noreply.github.com>
2020-05-19 14:18:33 +0200

    implemented #22

--- src/generate/random/GameOperation_random.cpp
+++ src/generate/random/GameOperation_random.cpp
@@ -3,10 +3,10 @@
 //
 
 #include "../GameOperation_gen.hpp"
+#include <gameLogic/generation/ActionGenerator.hpp>
 
 std::shared_ptr<spy::gameplay::BaseOperation>
-GameOperation_gen::random(const spy::util::UUID &/*characterId*/, const spy::gameplay::State &/*s*/,
-                          const spy::MatchConfig &/*config*/) {
-    // TODO implement
-    return nullptr;
+GameOperation_gen::random(const spy::util::UUID &characterId, const spy::gameplay::State &s,
+                          const spy::MatchConfig &config) {
+    return spy::gameplay::ActionGenerator::generateRandomAction(s, characterId, config);
 }
