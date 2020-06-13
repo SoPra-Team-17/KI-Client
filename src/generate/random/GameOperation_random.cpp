6a10b882c419affe27e422dbb3bfd351464d3cbc
csacro <50425802+csacro@users.noreply.github.com>
2020-06-13 13:08:08 +0200

    moved random generation from LibCommon to AI

--- src/generate/random/GameOperation_random.cpp
+++ src/generate/random/GameOperation_random.cpp
@@ -3,10 +3,12 @@
 //
 
 #include "../GameOperation_gen.hpp"
-#include <gameLogic/generation/ActionGenerator.hpp>
+#include <util/GameLogicUtils.hpp>
+#include <generate/utils/generate/OperationGenerator.hpp>
 
 std::vector<std::shared_ptr<spy::gameplay::BaseOperation>>
 GameOperation_gen::random(const spy::util::UUID &characterId, const spy::gameplay::State &s,
                           const spy::MatchConfig &config) {
-    return {spy::gameplay::ActionGenerator::generateRandomAction(s, characterId, config)};
+    spy::gameplay::State_AI state {s};
+    return {*spy::util::GameLogicUtils::getRandomItemFromContainer(OperationGenerator::generate(state, characterId, config))};
 }
